773b236a2f34d5c22443058aa403703e17742345
Sebastian Rietsch <sebrie75@gmail.com>
2020-08-31 09:59:30 +0200

    Bugfixes

--- dataset/dataset_tool.py
+++ dataset/dataset_tool.py
@@ -22,9 +22,11 @@ def predict_labels():
     pathlib.Path(pred_dir).mkdir(parents=True, exist_ok=True)
 
     for image_name in tqdm(image_names):
-        ext = image_name.split('.')[1]
+        basename, ext = get_base_ext(image_name)
+
         if ext not in ['jpg', 'jpeg', 'png']:
             continue
+
         # Predict
         image = Image.open(os.path.join(image_dir, image_name))
         image_tensor = ToTensor()(image).unsqueeze(0)
@@ -42,8 +44,8 @@ def clean():
 
     removed = 0
     for image_name in image_names:
-        # Remove videos
-        ext = image_name.split('.')
+        basename, ext = get_base_ext(image_name)
+
         if len(ext) < 2 or ext[1] not in ['jpg', 'jpeg', 'png']:
             os.remove(opjoin(image_dir, image_name))
             removed += 1
@@ -78,7 +80,7 @@ def filter():
 
     supervisions = 0
     for image_name in tqdm(image_names):
-        basename, ext = image_name.split('.')
+        basename, ext = get_base_ext(image_name)
 
         # Load prediction
         prediction = pickle.load(open(os.path.join(pred_dir, basename + '.pkl'), 'rb'))
@@ -99,63 +101,69 @@ def crop():
     # Create crop directory
     pathlib.Path(crop_dir).mkdir(parents=True, exist_ok=True)
     pathlib.Path(supervision_dir).mkdir(parents=True, exist_ok=True)
-
     for image_name in tqdm(image_names):
-        basename, ext = image_name.split('.')
+        basename, ext = get_base_ext(image_name)
 
         # Open image
         im = Image.open(opjoin(image_dir, image_name))
+        center_crop = False
 
         if os.path.isfile(opjoin(pred_dir, basename + '.pkl')):
             # Load prediction
             prediction = pickle.load(open(opjoin(pred_dir, basename + '.pkl'), 'rb'))
 
             # Find predictions > probability threshold
-            idxs = np.argwhere((prediction['labels'] == 18) & (prediction['scores'] > args.filter)).flatten()
+            p_thresh = 0.9
+            if args.filter != -1.0:
+                p_thresh = args.filter
+            idxs = np.argwhere((prediction['labels'] == 18) & (prediction['scores'] > p_thresh)).flatten()
 
-            # If unequal to 1, supervision is needed
+            # If unequal to 1, supervision is needed. Here we just do a center crop
             if len(idxs) != 1:
-                os.rename(opjoin(image_dir, image_name), opjoin(supervision_dir, image_name))
-                continue
-
-            # Image dimensions
-            w, h = im.size
-            min_side_img = min(w, h)
-
-            # Get bbox
-            idx = idxs[0]
-            bbox = prediction['boxes'][idx]
-
-            # Scale to at least 1024 pixels
-            if min_side_img < 1024:
-                scale_f = 1024 / min_side_img
-                im = im.resize((int(w * scale_f), int(h * scale_f)))
+                center_crop = True
+                #os.rename(opjoin(image_dir, image_name), opjoin(supervision_dir, image_name))
+            else:
+                # Image dimensions
                 w, h = im.size
                 min_side_img = min(w, h)
-                bbox *= scale_f
 
-            # Calculate longer side of bbox
-            max_side_bbox = int(max(bbox[2] - bbox[0], bbox[3] - bbox[1]))
+                # Get bbox
+                idx = idxs[0]
+                bbox = prediction['boxes'][idx]
 
-            # Ensure that crop is at least 1024, fits into image and,
-            # if possible, has size max_side_bbox + max_size_img/10
-            crop_size = min(min_side_img, max(1024, max_side_bbox + min_side_img / 10))
+                # Scale to at least 1024 pixels
+                if min_side_img < 1024:
+                    scale_f = 1024 / min_side_img
+                    im = im.resize((int(w * scale_f), int(h * scale_f)))
+                    w, h = im.size
+                    min_side_img = min(w, h)
+                    bbox *= scale_f
 
-            # Try to center crop around bbox center
-            bbox_center_x = bbox[0] + (bbox[2] - bbox[0]) / 2
-            bbox_center_y = bbox[1] + (bbox[3] - bbox[1]) / 2
-            crop_x = max(0, bbox_center_x - crop_size / 2)
-            crop_y = max(0, bbox_center_y - crop_size / 2)
+                # Calculate longer side of bbox
+                max_side_bbox = int(max(bbox[2] - bbox[0], bbox[3] - bbox[1]))
 
-            # Adjust in case the bbox is partially outside the image
-            crop_x_over = w - (crop_x + crop_size)
-            crop_y_over = h - (crop_y + crop_size)
-            if crop_x_over < 0:
-                crop_x += crop_x_over
-            if crop_y_over < 0:
-                crop_y += crop_y_over
+                # Ensure that crop is at least 1024, fits into image and,
+                # if possible, has size max_side_bbox + max_size_img/10
+                crop_size = min(min_side_img, max(1024, max_side_bbox + min_side_img / 10))
+
+                # Try to center crop around bbox center
+                bbox_center_x = bbox[0] + (bbox[2] - bbox[0]) / 2
+                bbox_center_y = bbox[1] + (bbox[3] - bbox[1]) / 2
+                crop_x = max(0, bbox_center_x - crop_size / 2)
+                crop_y = max(0, bbox_center_y - crop_size / 2)
+
+                # Adjust in case the bbox is partially outside the image
+                crop_x_over = w - (crop_x + crop_size)
+                crop_y_over = h - (crop_y + crop_size)
+                if crop_x_over < 0:
+                    crop_x += crop_x_over
+                if crop_y_over < 0:
+                    crop_y += crop_y_over
 
         else:
+            center_crop = True
+
+        if center_crop:
             # Image dimensions
             w, h = im.size
             min_side_img = min(w, h)
@@ -174,7 +182,8 @@ def crop():
         # Crop, resize (only if specified via argument) and save
         im_crop = im.crop((crop_x, crop_y, crop_x + crop_size, crop_y + crop_size))
         if args.crop is not None:
-            im_crop.resize((args.crop, args.crop))
+            im_crop = im_crop.resize((args.crop, args.crop))
+        im_crop = im_crop.convert('RGB')
         im_crop.save(os.path.join(crop_dir, image_name), "JPEG")
 
 def remove_duplicates():
@@ -204,6 +213,17 @@ def read_image_dir():
     image_names = [x for x in os.listdir(image_dir) if not os.path.isdir(opjoin(image_dir, x))]
     return image_names
 
+def get_base_ext(file_name):
+    # In case filename has dots in it
+    basename = ''
+    split = file_name.split('.')
+    for i in range(len(split) - 1):
+        basename += split[i] + '.'
+    basename = basename[:-1]
+    ext = split[-1]
+    return basename, ext
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='stylegan2 dataset tool.')
     parser.add_argument('image_dir')
@@ -219,14 +239,16 @@ if __name__ == '__main__':
     supervision_dir = os.path.join(image_dir, 'supervision')
     crop_dir = os.path.join(image_dir, 'crop')
     if args.crop is not None:
-        crop_dir += f'{crop_dir}_{args.crop}'
+        crop_dir = os.path.join(image_dir, f'crop_{args.crop}')
 
     print(args)
     if args.clean != -1:
         clean()
     if args.predict:
         predict_labels()
-    if args.filter != -1.0:
-        filter()
     if args.crop != -1.0:
         crop()
+    elif args.filter != -1.0:
+        filter()
+    if args.remove_duplicates:
+        remove_duplicates()
